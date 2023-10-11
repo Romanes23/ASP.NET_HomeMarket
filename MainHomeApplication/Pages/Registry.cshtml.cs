1d48fe89672dd15c0a937c89c10210227d375c20
Romanes23 <revtuhov@yandex.ru>
2023-10-11 20:53:51 +0300

    Добавлена форма для регистрации

---
+++ MainHomeApplication/Pages/Registry.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MainHomeApplication.Pages
+{
+    public class RegistryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
